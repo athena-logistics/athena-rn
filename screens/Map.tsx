50b7a2e50b42b2737a52a8561a3442f31c5ae1f0
Jonatan Männchen <jonatan@maennchen.ch>
2023-04-30 22:21:35 +0200

    Replace Event Map for Aufgetischt 2023 (#85)

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -8,19 +8,19 @@ const Map = () => {
       //   maxZoom={2}
       //   minZoom={0.1}
       maxZoom={1.5}
-      minZoom={0.15}
-      zoomStep={0.5}
-      initialZoom={0.15}
+      minZoom={0.05}
+      zoomStep={0.05}
+      initialZoom={0.1}
       // // Give these to the zoomable view so it can apply the boundaries around the actual content.
       // // Need to make sure the content is actually centered and the width and height are
       // // dimensions when it's rendered naturally. Not the intrinsic size.
       // // For example, an image with an intrinsic size of 400x200 will be rendered as 300x150 in this case.
       // // Therefore, we'll feed the zoomable view the 300x150 size.
-      contentWidth={2200}
-      contentHeight={2734}
+      contentWidth={4000}
+      contentHeight={5658}
     >
       <View>
-        <Image source={require('../assets/buskers-map.jpg')} />
+        <Image source={require('../assets/aufgetischt-map.jpg')} />
       </View>
     </ReactNativeZoomableView>
   );
