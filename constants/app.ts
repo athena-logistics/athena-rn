d2b245412f483452ed996345253a21798da48957
Jonatan Männchen <jonatan@maennchen.ch>
2023-05-17 19:34:31 +0200

    Fix Sentry DSN

--- constants/app.ts
+++ constants/app.ts
@@ -6,4 +6,4 @@ export const GITHUB_URL = 'https://github.com/athena-logistics';
 export const PUNKAH_URL = 'https://github.com/punkah';
 export const MAENNCHEN_URL = 'https://github.com/maennchen';
 export const SENTRY_DSN =
-  'https://9502a8031f704cba9ab7e55bbf48651f@o4505198663565312.ingest.sentry.io/4505198734016512';
+  'https://ea465e90b36941f39745423c4280ce4a@o4505198663565312.ingest.sentry.io/4505198742142976';
