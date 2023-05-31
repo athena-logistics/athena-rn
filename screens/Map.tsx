68f387ef6899f1bdd462a93c68f81e82832e0877
Sebi <sebastian.wehrli@gmail.com>
2023-05-31 17:13:38 +0200

    Add Buskers Map (#108)

--- screens/Map.tsx
+++ screens/Map.tsx
@@ -8,17 +8,17 @@ export default function Map() {
       maxZoom={1.5}
       minZoom={0.05}
       zoomStep={0.05}
-      initialZoom={0.1}
+      initialZoom={0.15}
       // Give these to the zoomable view so it can apply the boundaries around the actual content.
       // Need to make sure the content is actually centered and the width and height are
       // dimensions when it's rendered naturally. Not the intrinsic size.
       // For example, an image with an intrinsic size of 400x200 will be rendered as 300x150 in this case.
       // Therefore, we'll feed the zoomable view the 300x150 size.
-      contentWidth={4000}
-      contentHeight={5658}
+      contentWidth={3307}
+      contentHeight={4677}
     >
       <View>
-        <Image source={require('../assets/aufgetischt-map.jpg')} />
+        <Image source={require('../assets/buskers-map.jpg')} />
       </View>
     </ReactNativeZoomableView>
   );
