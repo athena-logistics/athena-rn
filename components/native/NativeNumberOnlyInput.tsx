1248d37ace0219620b1d859a52a059caba904976
Jonatan Männchen <jonatan@maennchen.ch>
2025-05-08 23:41:13 +0200

    Replace Broken Number Input

--- components/native/NativeNumberOnlyInput.tsx
+++ components/native/NativeNumberOnlyInput.tsx
@@ -1,13 +1,13 @@
 import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
-import { ViewStyle } from 'react-native';
-import NumericInput from 'react-native-numeric-input';
+import { TouchableHighlight } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
 
 export default function NativeNumberOnlyInput(
   props: Omit<
-    React.ComponentProps<typeof NumericInput>,
+    React.ComponentProps<typeof TextInput>,
     | 'inputStyle'
     | 'containerStyle'
     | 'type'
@@ -18,29 +18,68 @@ export default function NativeNumberOnlyInput(
     | 'customIncIcon'
     | 'leftButtonBackgroundColor'
     | 'rightButtonBackgroundColor'
-  >,
+    | 'onChange'
+    | 'value'
+  > & {
+    onChange: (value: number) => void;
+    textColor?: string;
+    value: number;
+    minValue?: number;
+    maxValue?: number;
+  },
 ) {
   const textColor = props.textColor ?? colors.primary;
+
+  const {
+    onChange,
+    textColor: _,
+    value,
+    minValue,
+    maxValue,
+    ...restProps
+  } = props;
+
+  function onTextChanged(value: string) {
+    if (!props.onChange) return;
+    const numValue = parseInt(value.replace(/[^0-9]/g, ''));
+    if (minValue !== undefined && numValue <= minValue) return;
+    if (maxValue !== undefined && numValue >= maxValue) return;
+    props.onChange(numValue);
+  }
+
   return (
     <>
-      <NumericInput
-        textColor={textColor}
-        inputStyle={{ fontFamily: fonts.defaultFontFamilyBold } as ViewStyle}
-        containerStyle={{ flexShrink: 0, width: 150 }}
-        {...props}
-        type="plus-minus"
-        borderColor="transparent"
-        totalWidth={150}
-        totalHeight={70}
-        customDecIcon={
-          <Ionicons size={44} name={'remove-circle'} color={textColor} />
-        }
-        customIncIcon={
-          <Ionicons size={44} name={'add-circle'} color={textColor} />
-        }
-        leftButtonBackgroundColor={'transparent'}
-        rightButtonBackgroundColor={'transparent'}
+      <TouchableHighlight
+        underlayColor="none"
+        onPress={() => {
+          props.onChange((props.value || 0) - 1);
+        }}
+        disabled={(props.value || 0) <= 0}
+      >
+        <Ionicons size={44} name={'remove-circle'} color={textColor} />
+      </TouchableHighlight>
+      <TextInput
+        placeholder={'0'}
+        style={{
+          fontFamily: fonts.defaultFontFamilyBold,
+          fontSize: 30,
+          color: textColor,
+          width: 60,
+          textAlign: 'center',
+        }}
+        keyboardType="numeric"
+        onChangeText={(value) => onTextChanged(value)}
+        value={props.value.toString()}
+        {...restProps}
       />
+      <TouchableHighlight
+        underlayColor="none"
+        onPress={() => {
+          props.onChange((props.value || 0) + 1);
+        }}
+      >
+        <Ionicons size={44} name={'add-circle'} color={textColor} />
+      </TouchableHighlight>
     </>
   );
 }
